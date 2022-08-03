708c3e39430f207e70c84d1c7ccc4cd84e8b19f0
Guru <gurunarayanancse@gmail.com>
2022-08-03 18:24:59 +0530

    Product Detail Screen

--- lib/QRScreen/QRScreen.dart
+++ lib/QRScreen/QRScreen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:star_link/StarLinkColor.dart';
-import 'package:star_link/customWidgets/StarLinkText.dart';
+
+import 'SupportWidgets/QRBody.dart';
+import 'SupportWidgets/QRHeader.dart';
 
 class QRScreen extends StatefulWidget {
   const QRScreen({Key? key}) : super(key: key);
@@ -16,42 +16,9 @@ class _QRScreenState extends State<QRScreen> {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [const QRHeader()],
+          children: const [QRHeader(), QRBody()],
         ),
       ),
-    );
-  }
-}
-
-class QRHeader extends StatelessWidget {
-  const QRHeader({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.arrow_back,
-              color: StarLinkColors.homePrimaryColor,
-            )),
-        Expanded(
-            child: Center(
-          child: StarLinkTextLabel(
-            text: "SCAN QR",
-            fontWeight: FontWeight.w600,
-            size: 17,
-            color: StarLinkColors.homePrimaryColor,
-          ),
-        )),
-        const SizedBox(
-          width: 70,
-        )
-      ],
     );
   }
 }
---
+++ lib/QRScreen/SupportWidgets/QRBody.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QRBody extends StatefulWidget {
+  const QRBody({Key? key}) : super(key: key);
+
+  @override
+  State<QRBody> createState() => _QRBodyState();
+}
+
+class _QRBodyState extends State<QRBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
